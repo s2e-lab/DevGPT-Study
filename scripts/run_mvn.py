49a75c37f32d7208499ca16f18e38f49d45bd9b2
Latif Siddiq Sunny <lsiddiqsunny@gmail.com>
2023-12-03 18:17:12 -0500

    updated run_mvn.py code

--- scripts/run_mvn.py
+++ scripts/run_mvn.py
@@ -5,7 +5,7 @@ import os
 import time
 import csv
 
-maven_project_path = "Java_maven"
+maven_project_path = "/Users/lsiddiqsunny/Documents/Notre_Dame/Research/DevGPT-Study/Java_maven"
 
 tested_projects = set()  # Initialize an empty set to keep track of tested projects
 output = []  # Accumulates all results
@@ -13,6 +13,8 @@ output = []  # Accumulates all results
 for root, dirs, files in os.walk(maven_project_path):
     for project in dirs:
         project_path = os.path.join(root, project)
+        print(f"Testing {project_path}")
+
 
         # Check if the directory starts with 'ignore-' or if it's already been tested
         if project.startswith("ignore-") or project in tested_projects:
@@ -33,18 +35,19 @@ for root, dirs, files in os.walk(maven_project_path):
 
             # Handle output or errors if needed
             print(process_output.decode('utf-8'))
-            print(process_error.decode('utf-8'))
 
             # Add the tested project to the set
             tested_projects.add(project)
 
             # Parse the compilation output for errors (example)
             # Modify this part based on your actual error parsing logic
-            lines = process_error.decode('utf-8').split('\n')
+            lines = process_output.decode('utf-8').split('\n')
             for line in lines:
                 if line.startswith("[ERROR]"):
                     output.append([project, line])  # Adjust output format as needed
+            print(f"Finished testing {project_path}")
 
+            print(output)
         except Exception as e:
             print(f"Error occurred: {e}")
 

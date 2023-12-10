0fd82785a666b66004080ffd4c323acaaaeda58d
Zhang Jiahao <michaelzjh2002@gmail.com>
2023-12-10 15:40:50 -0500

    update RQ2

--- scripts/RQ2/filter_unique_pr_url.py
+++ scripts/RQ2/filter_unique_pr_url.py
@@ -1,22 +1,15 @@
-def analyze_repos(file_path):
-    repo_counts = {}
+def extract_unique_urls(input_file, output_file):
+    with open(input_file, 'r') as file:
+        urls = file.readlines()
 
-    with open(file_path, 'r') as file:
-        for line in file:
-            parts = line.strip().split('/')
-            # Extract just the repository name
-            repo_name = parts[4] if len(parts) > 4 else None
-            if repo_name:
-                repo_counts[repo_name] = repo_counts.get(repo_name, 0) + 1
+    unique_urls = sorted(set(urls))
 
-    duplicates = [repo for repo, count in repo_counts.items() if count > 1]
-    unique_repo_count = len(repo_counts) - len(duplicates)
-
-    return unique_repo_count, duplicates
+    with open(output_file, 'w') as file:
+        for url in unique_urls:
+            file.write(url)
 
 
-file_path = 'pr_unique_url_python.txt'
-unique_repo_count, duplicate_repos = analyze_repos(file_path)
+input_file = 'pr_url_java.txt'
+output_file = 'pr_unique_url_java.txt'
 
-print(f"Number of unique repositories: {unique_repo_count}")
-print(f"Duplicate repositories: {duplicate_repos}")
+extract_unique_urls(input_file, output_file)
